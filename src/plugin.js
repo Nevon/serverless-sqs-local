47610aaf1c1705b111df03b97d913cb3352944c9
Tommy Brunn <tommy.brunn@klarna.com>
2017-07-16 21:09:14 +0200

    Use (almost) all attributes when creating queue

--- src/plugin.js
+++ src/plugin.js
@@ -9,12 +9,12 @@ class SqsLocalPlugin {
     this.log = message =>
       serverless.cli.log.bind(serverless.cli)(`SQS - ${message}`);
     this.options = options;
-    this.aws = aws
+    this.aws = aws;
     this.sqs = new aws.SQS({
-        endpoint: new aws.Endpoint(SQS_ENDPOINT),
-        accessKeyId: 'na',
-        secretAccessKey: 'na',
-        region: 'eu-west-1'
+      endpoint: new aws.Endpoint(SQS_ENDPOINT),
+      accessKeyId: "na",
+      secretAccessKey: "na",
+      region: "eu-west-1"
     });
   }
 
@@ -27,8 +27,9 @@ class SqsLocalPlugin {
             usage: "Creates queues from the current Serverless configuration."
           },
           remove: {
-              lifecycleEvents: ["removeHandler"],
-              usage: "Removes all queues listed in the current Serverless configuration."
+            lifecycleEvents: ["removeHandler"],
+            usage:
+              "Removes all queues listed in the current Serverless configuration."
           }
         }
       }
@@ -52,12 +53,16 @@ class SqsLocalPlugin {
   }
 
   removeHandler() {
-      this.log("Removing queues...");
-      const queuesInConfiguration = this.getQueues();
+    this.log("Removing queues...");
+    const queuesInConfiguration = this.getQueues();
 
-      return this.removeQueues(queuesInConfiguration).then(removed => {
-          removed.filter(n => n).forEach(removedQueue => this.log(`Deleted ${removedQueue}`));
-      }).catch(err => this.log(err));
+    return this.removeQueues(queuesInConfiguration)
+      .then(removed => {
+        removed
+          .filter(n => n)
+          .forEach(removedQueue => this.log(`Deleted ${removedQueue}`));
+      })
+      .catch(err => this.log(err));
   }
 
   getQueues() {
@@ -81,57 +86,110 @@ class SqsLocalPlugin {
     return Promise.all(queues.map(queue => this.createQueue(queue)));
   }
 
-  createQueue(queue) {
-    return new Promise((resolve, reject) => {
-      this.sqs.createQueue({
-          QueueName: queue.QueueName
-      }, function(err, data) {
-        if (err) {
-            reject(err);
-        } else {
-            resolve(data.QueueUrl);
+  resolveDeadLetterTargetArn(redrivePolicy) {
+    return this.resolve(redrivePolicy.deadLetterTargetArn);
+  }
+
+  createQueue({
+    QueueName,
+    ContentBasedDeduplication,
+    DelaySeconds,
+    FifoQueue,
+    MaximumMessageSize,
+    MessageRetentionPeriod,
+    ReceiveMessageWaitTimeSeconds,
+    RedrivePolicy,
+    VisibilityTimeout
+  }) {
+    let attributes = {
+      ContentBasedDeduplication,
+      DelaySeconds,
+      FifoQueue,
+      MaximumMessageSize,
+      MessageRetentionPeriod,
+      ReceiveMessageWaitTimeSeconds,
+      RedrivePolicy,
+      VisibilityTimeout
+    };
+
+    const deadLetterTargetArn = _.get(
+      attributes,
+      "RedrivePolicy.deadLetterTargetArn",
+      ""
+    );
+    if (_.isObject(deadLetterTargetArn)) {
+      this.log(
+        `Omitting RedrivePolicy for ${QueueName} as the serverless-sqs-local plugin is not able to resolve ${Object.keys(
+          deadLetterTargetArn
+        )[0]} 😞\nSee https://github.com/Nevon/serverless-sqs-local/issues/1`
+      );
+      attributes = _.omit(attributes, "RedrivePolicy");
+    }
+
+    attributes = _.reduce(
+      attributes,
+      (acc, key, val) => {
+        if (val !== undefined) {
+          acc[key] = `${JSON.stringify(val)}`;
+          return acc;
         }
-    })
-    });
+      },
+      {}
+    );
+
+    return new Promise((resolve, reject) =>
+      this.sqs.createQueue(
+        { QueueName, Attributes: attributes },
+        (err, data) => (err ? reject(err) : resolve(data.QueueUrl))
+      )
+    );
   }
 
   removeQueues(queues = []) {
-      return Promise.all(queues.map(queue => this.removeQueue(queue)));
+    return Promise.all(queues.map(queue => this.removeQueue(queue)));
   }
 
   removeQueue(queue) {
-      return this.getQueueUrl(queue.QueueName).then(url => {
-          return new Promise((resolve, reject) => {
-            this.sqs.deleteQueue({
-                QueueUrl: url
-            }, err => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(queue.QueueName)
-                }
-            })
-          })
-      }).catch(err => {
-          if (err.code === "AWS.SimpleQueueService.NonExistentQueue") {
-              this.log(`Queue ${queue.QueueName} does not exist. Skipping.`);
-          }
+    return this.getQueueUrl(queue.QueueName)
+      .then(url => {
+        return new Promise((resolve, reject) => {
+          this.sqs.deleteQueue(
+            {
+              QueueUrl: url
+            },
+            err => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(queue.QueueName);
+              }
+            }
+          );
+        });
       })
+      .catch(err => {
+        if (err.code === "AWS.SimpleQueueService.NonExistentQueue") {
+          this.log(`Queue ${queue.QueueName} does not exist. Skipping.`);
+        }
+      });
   }
 
   getQueueUrl(queueName) {
-      return new Promise((resolve, reject) => {
-          this.sqs.getQueueUrl({
-              QueueName: queueName,
-              QueueOwnerAWSAccountId: "na"
-          }, (err, data) => {
-              if (err) {
-                  reject(err)
-              } else {
-                  resolve(data.QueueUrl);
-              }
-          })
-      });
+    return new Promise((resolve, reject) => {
+      this.sqs.getQueueUrl(
+        {
+          QueueName: queueName,
+          QueueOwnerAWSAccountId: "na"
+        },
+        (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data.QueueUrl);
+          }
+        }
+      );
+    });
   }
 
   getDefaultStack() {
